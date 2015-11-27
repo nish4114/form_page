243538a659fc4e29780528533e46247cfbd8c0a0
nishantvarshney <nvarshney@adroit-inc.com>
2015-11-27 18:42:38 +0530

    mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'https://jobzgoform.herokuapp.com'}
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_mailer.raise_delivery_errors = true
