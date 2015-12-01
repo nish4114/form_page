71699cebf66c2d01b505bf67bc3e950a38e4ca79
nishantvarshney <nvarshney@adroit-inc.com>
2015-12-01 17:52:22 +0530

    uniquness

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,4 +1,6 @@
 class Form < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => 'That does not look like an email'
   validates_format_of :contact_no, :with => /\d{10}/, :message => 'Please enter 10 digits and without code'
+  validates_uniqueness_of :email, :message => 'This email is already registered'
+  validates_uniqueness_of :contact_no, :message => 'This phone number is already registered'
 end
