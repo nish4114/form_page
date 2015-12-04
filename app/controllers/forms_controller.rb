ffb25382d36e6362f88af55d930fdac532820334
nishantvarshney <nvarshney@adroit-inc.com>
2015-12-04 21:37:38 +0530

    sms text

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -18,7 +18,7 @@ class FormsController < ApplicationController
       @twilio_client.account.sms.messages.create(
           :from => "+1#{twilio_phone_number}",
           :to => "+91#{number_to_send_to}",
-          :body => "This is an message. It gets sent to #{number_to_send_to}"
+          :body => "Thank You for registering with JobzGo, We will contact you very soon"
       )
      rescue Twilio::REST::RequestError => e
        puts e.message
